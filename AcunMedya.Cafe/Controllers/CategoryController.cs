776d07a948809294f34b8dda256e4626c526ffc2
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2025-04-18 17:26:08 +0300

    17.04.2025

--- AcunMedya.Cafe/Controllers/CategoryController.cs
+++ AcunMedya.Cafe/Controllers/CategoryController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AcunMedya.Cafe.Context;
+using AcunMedya.Cafe.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AcunMedya.Cafe.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly CafeContext _context;
+
+        public CategoryController(CafeContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var values = _context.Categories.ToList();
             return View();
+        }
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            _context.Categories.Add(p);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category p)
+        {
+            _context.Categories.Update(p);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- AcunMedya.Cafe/Entities/Product.cs
+++ AcunMedya.Cafe/Entities/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace AcunMedya.Cafe.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AcunMedya.Cafe.Entities
 {
     public class Product
     {
@@ -8,6 +10,8 @@
         public string Description { get; set; }
         public string ImageUrl { get; set; }
 
+        [NotMapped] //veri tabanına kaydetmez
+        public IFormFile ImageFile { get; set; }    //kullanıcın yüklediği dosyayı temsil eder 
         public int CategoryId { get; set; }
         public Category Category { get; set; }
 
